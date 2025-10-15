464a87fc3b96f010df2e4a0a5709b6c1dc9d1edc
Joaquin <joaquinwilbert166@gmail.com>
2025-10-15 21:30:25 +0700

    integrated with abby

--- external-games-frontend/lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const FIXED_ROWS = 8 as const;
-export const FIXED_RISK = 'hard' as const;
--- external-games-frontend/lib/socket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client';
-
-const API = (process.env.NEXT_PUBLIC_API_BASE ?? 'http://localhost:3000').replace(/\/$/, '');
-
-export const socket = io(`${API}/ws/plinko`, { transports: ['websocket'] });
